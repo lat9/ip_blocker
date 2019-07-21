f2ece7da053ffe55d765f6bd7600fff50deb20e1
lat9 <lat9@vinosdefrutastropicales.com>
2019-07-21 08:13:43 -0400

    #14: Additional restructuring

--- special_login.php
+++ special_login.php
@@ -1,53 +1,52 @@
-<?php //Plugin: IP blocker
-require_once('includes/application_top.php');
+<?php
+// -----
+// Part of the "IP Blocker" plugin, provided by lat9 and torvista.
+// Copyright (C) 2015-2019, Vinos de Frutas Tropicales.
+//
+require 'includes/application_top.php';
 
-if (isset($_SESSION['ip_blocker_pass']) && $_SESSION['ip_blocker_pass'] == true) {
-    zen_redirect(zen_href_link(FILENAME_DEFAULT));  //-v2.0.2c
+if (!empty($_SESSION['ip_blocker_pass'])) {
+    zen_redirect(zen_href_link(FILENAME_DEFAULT));
 }
 
 if (isset($_SESSION['ip_blocker_fail'])) {
     exit();
-}  //-v2.0.1a
+}
 
 $message_show = false;
 
-if (isset($_POST) && isset($_POST['pwd'])) {
+if (isset($_POST['pwd'])) {
     $message_show = true;
 
-    $password = zen_db_prepare_input($_POST['pwd']);
+    $password = (string)$_POST['pwd'];
 
     if ($password == '') {
         $message = 'Password required !';
     } else {
         $password = md5(md5($password . '_secure_key'));
 
-        $lockout_field = ($sniffer->field_exists(TABLE_IP_BLOCKER, 'ib_lockout_count')) ? ', ib_lockout_count' : '';  //-v2.0.1a
-        $password_fields = $db->Execute("SELECT ib_password$lockout_field FROM " . TABLE_IP_BLOCKER . ' WHERE ib_id=1'); //-v2.0.1c
-        $ib_password = $password_fields->fields['ib_password'];  //-v2.0.1c
+        $password_fields = $db->Execute("SELECT * FROM " . TABLE_IP_BLOCKER . ' WHERE ib_id=1');
+        $ib_password = $password_fields->fields['ib_password'];
 
         if ($ib_password != '' && $password != $ib_password) {
             $message = 'Wrong password!';
-//-bof-v2.0.1a
-            if ($lockout_field != '' && $password_fields->fields['ib_lockout_count'] != 0) {
+
+            if ($password_fields->fields['ib_lockout_count'] != 0) {
                 if (!isset($_SESSION['ib_lockout_count'])) {
-                    $_SESSION['ib_lockout_count'] = 1;
-
-                } else {
-                    $_SESSION['ib_lockout_count']++;
-
+                    $_SESSION['ib_lockout_count'] = 0;
                 }
+                $_SESSION['ib_lockout_count']++;
+
                 if ($_SESSION['ib_lockout_count'] >= $password_fields->fields['ib_lockout_count']) {
                     $_SESSION['ip_blocker_fail'] = 1;
                     exit();
                 }
             }
-//-eof-v2.0.1a
 
         } elseif ($ib_password != '' && $password == $ib_password) {
             $_SESSION['ip_blocker_pass'] = true;
-            unset ($_SESSION['ib_lockout_count']);  //-v2.0.2a
-            zen_redirect(zen_href_link(FILENAME_DEFAULT));  //-v2.0.2c-Use built in zen functions
-
+            unset($_SESSION['ib_lockout_count']);
+            zen_redirect(zen_href_link(FILENAME_DEFAULT));
         }
     }
 }
@@ -99,7 +98,7 @@ if (isset($_POST) && isset($_POST['pwd'])) {
         <form action="#" name="i_l" method="POST" target="_self">
             <div class="d_p">
                 <label for="pwd">Password</label>
-                <input type="password" name="pwd" id="pwd" size="35" value=""/>
+                <input type="password" name="pwd" id="pwd" size="35" value="" />
                 <input type="submit" value="Login"/></div>
             <?php
             if ($message_show) {
@@ -113,4 +112,5 @@ if (isset($_POST) && isset($_POST['pwd'])) {
     </div>
     </body>
     </html>
-<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
+<?php 
+require DIR_WS_INCLUDES . 'application_bottom.php';
